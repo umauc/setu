32df82a949821fe946e979d8211ca6a1a4f5212e
0x7FFFFF <w13657804853@outlook.com>
2020-08-15 02:30:01 +0800

    Update setu.py

--- setu.py
+++ setu.py
@@ -48,9 +48,9 @@ class setu_get(object):
             self.url = pic_data.get('illust').get('image_urls').get('large').replace('pximg.net','pixiv.cat')
             return {'pid':self.pid,'title':self.title,'url':self.url}
         except:
-            pic_data = requests.get(f'https://api.imjad.cn/pixiv/v1/?type=illust&id={self.pid}').json().replace('response','illust')[0]
-            self.title = pic_data.get('illust').get('title')
-            self.url = pic_data.get('illust').get('image_urls').get(
+            pic_data = requests.get(f'https://api.imjad.cn/pixiv/v1/?type=illust&id={self.pid}').json()
+            self.title = pic_data.get('response')[0].get('title')
+            self.url = pic_data.get('response')[0].get('image_urls').get(
                 'medium').replace('pximg.net', 'pixiv.cat')
             return {'pid':self.pid,'title':self.title,'url':self.url}
 
@@ -438,15 +438,15 @@ async def setu_upload(app: Mirai, message: MessageChain, friend: Friend):
                 except:
                     pass
             all_images_pid_r18 = []
-            try:
-                await app.sendFriendMessage(user,[Plain(text='R18识别')])
-                print(friend.nickname+'：R18识别')
-                for i in all_images_pid:
-                    if api.illust_detail(i).get('illust').get('tags')[0].get('name') == 'R-18':
-                        all_images_pid_r18.append(i)
-                        all_images_pid.remove(i)
-            except:
-                raise
+            #try:
+            #    await app.sendFriendMessage(user,[Plain(text='R18识别')])
+            #    print(friend.nickname+'：R18识别')
+            #    for i in all_images_pid:
+            #        if api.illust_detail(i).get('illust').get('tags')[0].get('name') == 'R-18':
+            #            all_images_pid_r18.append(i)
+            #            all_images_pid.remove(i)
+            #except:
+            #    raise
         except:
             pass
         try:
